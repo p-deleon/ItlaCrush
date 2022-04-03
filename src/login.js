702dc65a6cb143379621411aa949659b5655ef8a
p-deleon <202010612@itla.edu.do>
2022-04-02 22:12:35 -0400

    first commit

---
+++ src/login.js
@@ -0,0 +1,17 @@
+export default function login()
+{
+    return (
+        
+            <div className="login">
+               <input placeholder="Nombre de Usuario"/>
+               <br></br>
+               <br></br>
+               <input placeholder="Contraseña"/>
+               <br></br>
+               <br></br>
+               <button>Log in</button>
+
+            </div>
+       
+    )
+}
---
+++ src/nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function nav()
+{
+    return(
+       <div>
+            <ul>
+          
+          <li>Home</li>
+          <li>Login</li>
+          <li>About</li>
+      </ul>
+       </div>
+    );
+}
